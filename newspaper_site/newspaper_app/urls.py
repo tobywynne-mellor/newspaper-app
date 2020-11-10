840e19d8b9e092f2973a35b030d54a1540d11bad
avrs-git <avrs.git@yandex.com>
2020-11-10 19:38:51 +0000

    Added general index style.

--- newspaper_site/newspaper_app/urls.py
+++ newspaper_site/newspaper_app/urls.py
@@ -5,14 +5,9 @@ app_name = "newspaper_app"
 urlpatterns = [
     path("", views.Articles_view, name="index"),
     path("article/<int:id>/", views.Article_view, name="article"),
-    path('register_validation/', views.register_validation,
-         name="register_validation"),
-    path('register/', views.register_view,
-         name="register_form"),
-    path('login_validation/', views.login_validation,
-         name="login_validation"),
-    path('login/', views.login_view,
-         name="login_form"),
-    path('logout/', views.logout_view,
-         name="logout")
+    path('register_validation/', views.register_validation, name="register_validation"),
+    path('register/', views.register_view, name="register_form"),
+    path('login_validation/', views.login_validation, name="login_validation"),
+    path('login/', views.login_view, name="login_form"),
+    path('logout/', views.logout_view, name="logout")
 ]
