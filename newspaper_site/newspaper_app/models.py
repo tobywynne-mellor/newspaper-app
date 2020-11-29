7313a6f378bca8950c1e3c42c3eeb0a5bb039f7a
avrs-git <avrs.git@yandex.com>
2020-11-29 03:52:29 +0000

    Default pic, error fix for image resizing, error fixes for jQuery image control

--- newspaper_site/newspaper_app/models.py
+++ newspaper_site/newspaper_app/models.py
@@ -30,26 +30,28 @@ class Article(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
     profile_pic = models.ImageField(blank=True, null=True)
 
-    def save(self):
-        resized_pic = Image.open(self.profile_pic)
-        output = BytesIO()
-        resized_pic = resized_pic.resize((200,200))
-        resized_pic.save(output, format='PNG', quality=100)
-        output.seek(0)
-        self.profile_pic = InMemoryUploadedFile(output,'ImageField', "%s.png" %self.profile_pic.name.split('.')[0], 'image/png', sys.getsizeof(output), None)
-        super(Profile,self).save()
-
     pref_cate = models.ManyToManyField(Category)
-    dob = models.DateField(default=datetime.date.today,
-                           null=True, max_length=8)
+    dob = models.DateField(default=datetime.date.today, null=True, max_length=8)
     email = models.EmailField(max_length=254)
+
+    def save(self):
+        super(Profile,self).save()
+        if self.profile_pic:
+            resized_pic = Image.open(self.profile_pic)
+            output = BytesIO()
+            resized_pic = resized_pic.resize((200,200))
+            resized_pic.save(output, format='PNG')
+            output.seek(0)
+            self.profile_pic = InMemoryUploadedFile(output,'ImageField', "%s.png" %self.profile_pic.name.split('.')[0], 'image/png', sys.getsizeof(output), None)
+            super(Profile,self).save()
 
     def __str__(self):
         return self.user.username
 
-
+    
 class Like(models.Model):
 
     article = models.ForeignKey(
