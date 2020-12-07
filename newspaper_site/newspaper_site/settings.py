d326df25f94c9bd92972a87d60014589dada5c2b
Toby <tobywynnemellor@gmail.com>
2020-12-07 11:14:17 +0000

    turn off debug

--- newspaper_site/newspaper_site/settings.py
+++ newspaper_site/newspaper_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*c&&c=_g@2s9f@03q*@g99yk%c6d*ei@37-@&4uce!i1dy0_-q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.apps.okd.eecs.qmul.ac.uk', 'localhost', '127.0.0.1']
 
@@ -43,13 +43,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'allow_cidr.middleware.AllowCIDRMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'newspaper_site.urls'
